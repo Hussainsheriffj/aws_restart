1707a88aaacb095178a78a00a85447306e85610b
Hussain sheriff J <hussain.j1994@gmail.com>
2022-05-15 02:11:53 +0530

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,16 @@
+myvalue = 1
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5J
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
